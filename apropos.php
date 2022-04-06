ef48149e0d99247d6cf628c55f16e649b77ac030
Ninaxtrem <nina@starxtrem.com>
2022-04-06 13:13:16 +0200

    apropos fini

--- apropos.php
+++ apropos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stls.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"/>
@@ -15,6 +15,7 @@
     <title>Carolo gameshow - Simplon </title>
 </head>
 <body>
+<!-- nav -->
 <nav>
             <img src="media/images/logo-simplon.png" class="logo">
             <ul>
@@ -22,6 +23,41 @@
                 <li><a href="#">Jeux</a></li>
                 <li><a href="#">A propos de nous</a></li>
             </ul>
-        </nav>
+</nav>
+<!-- fin nav -->
+<!--simplon--> 
+  <div class="contenaire">
+    <div class="box1">
+        <img class="img" src="media/images/logo-simplon.png">
+    <p>
+    Simplon.co est un réseau de Fabriques solidaires inclusives
+qui proposent des formations gratuites aux métiers techniques
+du numérique en France et à l’étranger. </p>
+    </div>
+    <div class="box2">
+      <img class="img" src="media/images/UIMM.png">
+        <p>
+        Dans un contexte concurrentiel et de recherche de compétitivité, 
+        la complexité administrative, législative et réglementaire fait que le chef d’entreprise a 
+        plus que jamais besoin de sortir de son isolement, d’être défendu et accompagné.</p>
+      </div>
+    </div>
+  </div>
+<!--fin simplon-->   
+<!--mario-->
+<div class="bar">
+<div class="sky">
+    <img class="cloud" src="http://shimotmk.com/wp-content/uploads/2019/07/cloud.png" alt="">
+    <img class="cloud" src="http://shimotmk.com/wp-content/uploads/2019/07/cloud.png" alt="">
+  </div>
+  <div class="grass"></div>
+  <div class="road">
+    <div class="lines"></div>
+    <img class="mario" src="http://shimotmk.com/wp-content/uploads/2019/07/mario.png" alt="">
+    <img class="luigi" src="http://shimotmk.com/wp-content/uploads/2019/07/luigi.png" alt="">
+  </div>
+</div>
+<!--fin mario-->
+<!--uim-->
 </body>
 </html>
